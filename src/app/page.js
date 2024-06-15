9ccd4e51a51018dd7b5795ad6776257f4503c598
Ayush-2302 <ayushkumarakt@gmail.com>
2024-06-15 16:53:51 +0530

    resolved bugs

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import Swiper from "@/components/Swiper";
 export default function Home() {
   return (
     <div className=" space-y-10">
+      <h1>Hello world</h1>
       <Swiper />
       <ImageBioSection />
       <CrouselHome2 />

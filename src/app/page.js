ae11b8116859b95805220934dba8fbed7b8b0a40
Ayush-2302 <ayushkumarakt@gmail.com>
2024-06-15 11:21:09 +0530

    resolved database issue and made api for updating user of noorpur

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,18 @@
 "use client";
-import CrouselHome from "@/components/CrouselHome";
 import CrouselHome2 from "@/components/CrouselHome2";
 import ImageBioSection from "@/components/ImageBioSection";
-import Tabs from "@/components/Tabs";
 import QuickLinks from "@/components/QuickLinks";
 import SocialAccountsProfileFrames from "@/components/SocialAccountsProfileFrames";
-import Aboutsection from "@/components/Aboutsection";
 import Swiper from "@/components/Swiper";
-import ContactForm from "@/components/ContactForm";
-import ContactNewForm from "@/components/ContactNewForm";
-import { useEffect, useState } from "react";
-import BarcodeGenerator from "@/components/BarcodeGenerator";
 
 export default function Home() {
-  return <div className=" space-y-10">
-    <Swiper/>
-    {/* <CrouselHome/> */}
-    <ImageBioSection/>
-    <CrouselHome2/>
-    <QuickLinks/>
-      {/* <Aboutsection/> */}
-    <SocialAccountsProfileFrames/>
-    {/* <ContactForm /> */}
-    {/* <ContactNewForm /> */}
-    {/* <BarcodeGenerator /> */}
-  </div>;
+  return (
+    <div className=" space-y-10">
+      <Swiper />
+      <ImageBioSection />
+      <CrouselHome2 />
+      <QuickLinks />
+      <SocialAccountsProfileFrames />
+    </div>
+  );
 }

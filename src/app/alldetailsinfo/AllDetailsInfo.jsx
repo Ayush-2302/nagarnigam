7b1613c9119d8eda952164577d69abeda672d8b0
Ayush-2302 <ayushkumarakt@gmail.com>
2024-07-13 08:48:10 +0530

    change folders

--- src/app/alldetailsinfo/AllDetailsInfo.jsx
+++ src/app/alldetailsinfo/AllDetailsInfo.jsx
@@ -1,4 +1,4 @@
-\"use client";
+"use client";
 import React, { useEffect, useState } from "react";
 import { fetchAllUserDetails } from "@/helper/apiservices/fetchUserDetails";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";

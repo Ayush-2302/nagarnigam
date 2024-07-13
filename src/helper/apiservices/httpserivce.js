a1267c525fbd52cfbe460b381a5cc2902878dd60
Ayush-2302 <ayushkumarakt@gmail.com>
2024-07-13 08:53:03 +0530

    change folders

--- src/helper/apiservices/httpserivce.js
+++ src/helper/apiservices/httpserivce.js
@@ -1,2 +1,2 @@
-export const httpService = "https://nagarnigam-peach.vercel.app/";
+export const httpService = "https://nagarnigam-peach.vercel.app/api";
 // export const httpService = "http://localhost:3000/api";
